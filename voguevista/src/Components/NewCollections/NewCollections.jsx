0facc32004a4c980f3d62439958cfcd4ed428d65
itsnitu028 <128230467+itsnitu028@users.noreply.github.com>
2024-08-29 04:07:38 +0530

    Update NewCollections.jsx

--- voguevista/src/Components/NewCollections/NewCollections.jsx
+++ voguevista/src/Components/NewCollections/NewCollections.jsx
@@ -9,7 +9,7 @@ const NewCollections = () => {
   const [new_collection,setNew_collection]=useState([]);
 
   useEffect(()=>{
-    fetch('https://voguevista-backend.onrender.com/newcollections/newcollections')
+    fetch('https://voguevista-backend.onrender.com/newcollections')
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data));
 },[])
