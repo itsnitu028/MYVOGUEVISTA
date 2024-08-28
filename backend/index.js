56f5de3ca0004bb32d13a87beba89b1b0847894b
itsnitu028 <128230467+itsnitu028@users.noreply.github.com>
2024-08-29 03:57:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,7 @@ const cors=require('cors');
 
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:5173',
-    origin:'http://localhost:3000',
-    origin:'https://voguevista-backend.onrender.com'
+    origin:"*"
 
 }));
 
